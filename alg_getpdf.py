1b1e89120ee11a65317588cda084ea66f10877fd
Gjermund Ask <gjerma14@student.uia.no>
2015-09-24 02:25:19 +0200

    Choose to get individual chapters or whole book, add .gitignore

--- alg_getpdf.py
+++ alg_getpdf.py
@@ -20,32 +20,73 @@ content = soup.find('div', attrs={'class': 'tekst'}).find_all('div')[1].find_all
 def append_pdf(input,output):
     [output.addPage(input.getPage(page_num)) for page_num in range(input.numPages)]
 
-for c in content:
-	c_page = urllib2.urlopen('http://www.iu.hio.no/~ulfu/appolonius/' + c.get('href')).read()
-	soup_page = BeautifulSoup(c_page, 'lxml')
-	pdf_url = soup_page.find_all('a', attrs={'href': re.compile('.pdf')})
-	for p in pdf_url:
-		file_name = p.get('href')
-		if len(file_name) == 12:
-			download_list.append('http://www.iu.hio.no/~ulfu/appolonius/kap' + file_name[5] + file_name[6] + "/" + p.get('href'))
-		elif len(file_name) == 11:
-			download_list.append('http://www.iu.hio.no/~ulfu/appolonius/kap' + file_name[5] + "/" + p.get('href'))
-		
-output = PdfFileWriter()
+def get_book():
+	for c in content:
+		dl_this = []
+		c_page = urllib2.urlopen('http://www.iu.hio.no/~ulfu/appolonius/' + c.get('href')).read()
+		soup_page = BeautifulSoup(c_page, 'lxml')
+		pdf_url = soup_page.find_all('a', attrs={'href': re.compile('.pdf')})
+		for p in pdf_url:
+			file_name = p.get('href')
+			if len(file_name) == 12:
+				dl_this.append('http://www.iu.hio.no/~ulfu/appolonius/kap' + file_name[5] + file_name[6] + "/" + p.get('href'))
+			elif len(file_name) == 11:
+				dl_this.append('http://www.iu.hio.no/~ulfu/appolonius/kap' + file_name[5] + "/" + p.get('href'))
+		download_list.append(dl_this)
 
-for url in download_list:
-	filename = url.split('/')[-1]
+
+def get_chapters():
+	for url in download_list:
+		output = PdfFileWriter()
+		errored = False
+		if url:
+			filename = url[0].split('/')[-1]
+			p = re.compile('(?<=kap)\d{1,3}')
+			chap = p.search(filename).group()
+			chap = chap[:-1]
+			print "Doing chapter", chap
+			for u in url:
+				try: 
+					pdf = urllib2.urlopen(u).read()
+					mem_file = StringIO(pdf)
+					append_pdf(PdfFileReader(mem_file), output)
+				except Exception, e:
+					print "Error for chapter " + chap + ": " + str(e)
+					errored = True
+					pass
+			try:
+				if not errored: 
+					output.write(file("algs-kap" + chap + ".pdf", "wb"))
+					print "Assembled pdf at algs-"+ chap + ".pdf"
+				else:
+					print "Couldn't get chapter, not assembled"
+
+			except Exception, e:
+				print "Error ocurred!"
+				print e
+
+def get_full_book_pdf():
+	output = PdfFileWriter()
+	for url in download_list:
+		if url:
+			for u in url:
+				try: 
+					pdf = urllib2.urlopen(u).read()
+					mem_file = StringIO(pdf)
+					append_pdf(PdfFileReader(mem_file), output)
+				except Exception, e:
+					print "Error for chapter; " + str(e)
+					pass
 	try:
-		pdf = urllib2.urlopen(url).read()
-		mem_file = StringIO(pdf)
-		append_pdf(PdfFileReader(mem_file), output)
-		print "Got", filename
-	except:
-		pass
+		output.write(file("algs-full.pdf", "wb"))
+		print "Assembled pdf at algs-full.pdf"
 
-try:
-	output.write(file("algs.pdf", "wb"))
-	print "Assembled pdf at algs.pdf"
-except Exception, e:
-	print "Error ocurred!"
-	print e
+	except Exception, e:
+			print "Error ocurred!"
+			print e
+
+
+
+get_book()
+#get_chapters()
+get_full_book_pdf()
