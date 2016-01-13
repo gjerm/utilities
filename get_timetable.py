8d2ef92777eeaa1eb6cb8b94bf3e27361b0b5f3b
Gjermund Ask <gjerma14@student.uia.no>
2016-01-13 19:52:18 +0100

    Fields in order: weekday, date, start time, end time, subject code, type, extra info, room code(s).

--- get_timetable.py
+++ get_timetable.py
@@ -4,12 +4,17 @@ from bs4 import BeautifulSoup
 from time import sleep
 from urllib2 import urlopen
 from collections import OrderedDict
+import re
 
 RADIO_TYPE = "XMLSpreadsheet;studentsetxmlurl;SWSCUST+StudentSet+XMLSpreadsheet"
 
 # These are parameters that will be fetched from the page.
 auto_params = ["__EVENTTARGET", "__EVENTARGUMENT", "__LASTFOCUS", "__VIEWSTATE", "__VIEWSTATEGENERATOR", "__EVENTVALIDATION", "tLinkType",
 			  "tWildcard", 'bGetTimetable']
+
+day_convert = {"Man": "mon", "Tir": "tue", "Ons": "wed", "Tor": "thu", "Fre": "fri", "Lør": "sat"}
+rooms = re.compile('([A-D]\d \d{3})')
+non_digits = re.compile('[\D]+')
 
 def get_timetable(days, weeks, subject_code, season):
 	url = "http://timeplan.uia.no/swsuia" + season + "/public/no/default.aspx"
@@ -77,32 +82,72 @@ def get_subject_codes(season):
 def convert_to_table_format(html):
 	soup = BeautifulSoup(html, 'lxml')
 	tab = soup.find_all('table')
-
 	table = ""
-	counter = 0
 
 	for t in tab:
-		counter += 1
-		table += "Week " + str(counter) + "\n"
 		# print "Week " + str(counter)
 		for c in t:
+			week_info = c.find('td', {"class": "td1"})
+			if week_info:
+				table += week_info.getText().encode('utf-8') + "\n"
 			# Exclude if header of table
 			if not "tr1" in c.get('class'):
 				row = c.find_all('td')
-				for val in row:
+				for i in range(len(row)):
 					# Remove silly whitespace
-					if len(val.getText()) == 0:
+					if len(row[i].getText()) == 0:
 						table += "\n"
 					else:
-						table += val.getText().encode('utf-8').strip() + ";"
+						val = row[i].getText().encode('utf-8').strip()
+						# Convert weekdays
+						if i == 0:
+							val = day_convert[val]
+						if i == 2:
+							val = val.split("-")
+							table += val[0] + ";" + val[1] + ";"
+							continue
+						# Extract info like subject code, type of class
+						elif i == 3:
+							val = val[1:].split("/")
+							code = val[0]
+							typ = val[1]
+							info = ""
+							for _ in val[2:]:
+								if re.findall(non_digits, _):
+									info += _
+							table += code + ";" + typ + ";" + info + ";"
+							continue
+						# Find and extract rooms
+						elif i == 4:
+							listed_rooms = re.findall(rooms, val)
+							listed_rooms = "/".join(listed_rooms)
+							val = listed_rooms
+						# No need to add names of lecturers etc
+						elif i == 5:
+							continue
+
+						table += val + ";"
+
 	return table
 
-# A list of weeks to return (should start at 1 and not skip any weeks for now)
-weeks = range(1, 31)
+def process_table(tab):
+	
+	
+
+	for r in rows:
+		l = r.split(";")
+		listed_rooms = re.find_all(rooms, l[3])
+		listed_rooms = "/".join(listed_rooms)
+
+
+# "t" means this week
+#weeks = "t"
+weeks = range(1,31)
 
 # Can be 1-3 (mon-wed), 4-6 (thu-sat) or 1-6 (mon-sat)
 days = "1-6"
 
 # "v" for spring, "h" for autumn
 print get_timetable(days, weeks, "#SPLUSE0C745", "v")
-#print get_subject_codes("v")
+# subject_codes = get_subject_codes("v")
+# for k, v in subject_codes.items(): print k.encode('utf-8') + ": " + v.encode('utf-8')
